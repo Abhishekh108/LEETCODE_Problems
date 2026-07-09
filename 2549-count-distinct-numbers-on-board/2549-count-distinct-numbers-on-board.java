f134060a660f358674078c3b9bed378f65068b2f
Abhishekh Verma <kundanverma982155@gmail.com>
2026-07-09 18:23:18 +0545

    Time: 0 ms (100%), Space: 42.2 MB (25.65%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int distinctIntegers(int n) {
+        if (n==1) return n;
+        return n-1;
+    }
+}
+//n % n-1 === 1 always when n>1
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
