be79f1b69c9f31239b4bece9b8e84d9a345af48a
Abhishekh Verma <kundanverma982155@gmail.com>
2026-07-11 17:59:52 +0530

    Time: 33 ms (92.83%), Space: 94.9 MB (44.21%) - LeetHub - Time: O(n log n), Space: O(1)

---
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        long sum=0;
+        int max=0;
+        int i=0; int j=0;
+// 1 <= nums.length <= 10^5
+// 1 <= nums[i] <= 10^5
+// 10000000000
+// 2147483647
+        while(j<nums.length){
+            sum=sum+nums[j];
+
+            while((long)nums[j]*(j-i+1)-sum> k){
+                sum=sum-nums[i];
+                i++;
+            }
+            max= Math.max(max,j-i+1);
+
+            j++;
+        }
+        return max;
+    }
+
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
