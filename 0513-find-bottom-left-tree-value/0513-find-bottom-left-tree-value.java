87a39d85050f6188599fa472f269fd3a77c8297c
Abhishekh Verma <kundanverma982155@gmail.com>
2026-07-28 00:24:46 +0530

    Time: 0 ms (100%), Space: 46.5 MB (41.09%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    int ans = 0;
+    int maxLevel = -1;
+
+    public int findBottomLeftValue(TreeNode root) {
+        solve(root, 0);
+        return ans;
+    }
+
+    public void solve(TreeNode root, int level) {
+        if (root == null) {
+            return;
+        }
+
+        if (level > maxLevel) {
+            maxLevel = level;
+            ans = root.val;
+        }
+
+        solve(root.left, level + 1);
+        solve(root.right, level + 1);
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
