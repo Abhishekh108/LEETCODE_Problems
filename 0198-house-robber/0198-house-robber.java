9457ddd0bc177f66fe29626025f75ba87fb7059a
Abhishekh Verma <kundanverma982155@gmail.com>
2026-09-03 23:09:19 +0530

    Time: 0 ms (100%), Space: 42.9 MB (19.26%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minamount(int []arr,int i,int []dp){
+        // as it start from i=0 so i>= n then over
+        if(i>=arr.length)return 0;
+        //just below the base case
+        if(dp[i] !=-1) return dp[i];
+
+        int take= arr[i]+ minamount(arr,i+2,dp);
+        int skip= minamount(arr,i+1,dp);
+        return dp[i]=Math.max(take,skip);
+    }
+    public int rob(int[] nums) {
+        //make dp array
+        int dp[]= new int[nums.length];
+        Arrays.fill(dp,-1);
+        return minamount(nums,0,dp);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
