89782792e34ef322595e78d5fbfce50476bb5a77
Abhishekh Verma <kundanverma982155@gmail.com>
2026-09-04 09:52:32 +0530

    Time: 14 ms (50.63%), Space: 42 MB (72.89%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -0,0 +1,27 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int low=0;
+        int high=n;
+        boolean flag;
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            flag=isBadVersion(mid);
+            if(flag==true){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
