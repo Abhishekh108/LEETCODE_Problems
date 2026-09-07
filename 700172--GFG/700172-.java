fad71891c527cc1f75782f67124d59b16a1b7628
Abhishekh Verma <kundanverma982155@gmail.com>
2026-09-08 02:05:22 +0530

    Added solution

---
+++ 700172--GFG/700172-.java
@@ -0,0 +1,29 @@
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+*/
+class Solution {
+    boolean isCircular(Node head) {
+        // code here
+        Node slow= head;
+        Node fast= head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
