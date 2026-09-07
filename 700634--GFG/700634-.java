cc637e89c00ef486837b744a3dcf61772a89b66d
Abhishekh Verma <kundanverma982155@gmail.com>
2026-09-08 00:23:09 +0530

    Added solution

---
+++ 700634--GFG/700634-.java
@@ -0,0 +1,34 @@
+/*node class of the linked list is as:
+class Node {
+    int data;
+     Node next;
+     Node(int key)
+     {
+         data = key;
+         next = null;
+     }
+}
+*/
+
+class Solution {
+    public int modularNode(Node head, int k) {
+        // code here
+        int ans= -1;
+        int index= 1;
+        Node temp= head;
+        
+        while(temp != null){
+            if(index%k==0) {
+                ans=temp.data;
+                
+            }
+            temp=temp.next;
+            index++;
+        }
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
