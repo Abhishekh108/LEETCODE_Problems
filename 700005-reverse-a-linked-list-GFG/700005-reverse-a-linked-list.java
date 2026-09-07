ce907d0a273ae239bbd526c0b50e04defbd00357
Abhishekh Verma <kundanverma982155@gmail.com>
2026-09-08 01:26:13 +0530

    Added solution

---
+++ 700005-reverse-a-linked-list-GFG/700005-reverse-a-linked-list.java
@@ -0,0 +1,32 @@
+/* Structure of Linked List Node
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    Node reverseList(Node head) {
+        // code here
+        Node temp=head;
+        Node next=head;
+        Node prev=null;
+        while(next!=null){
+            next=next.next;
+            // change
+            temp.next=prev;
+            prev=temp;
+            temp=next;
+        }
+        return prev;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
