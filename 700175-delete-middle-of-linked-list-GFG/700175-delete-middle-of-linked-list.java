5ba1969e7b329b7322da23afe411bc1e1b13677d
Abhishekh Verma <kundanverma982155@gmail.com>
2026-09-08 14:49:21 +0530

    Added solution

---
+++ 700175-delete-middle-of-linked-list-GFG/700175-delete-middle-of-linked-list.java
@@ -0,0 +1,39 @@
+/* Node Structure
+class Node {
+    int data;
+    Node next;
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+} */
+
+class Solution {
+    Node deleteMid(Node head) {
+
+        // If there is only one node
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        Node slow = head;
+        Node fast = head;
+        Node prev = null;
+
+        // Find middle node
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Delete middle node
+        prev.next = slow.next;
+
+        return head;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
