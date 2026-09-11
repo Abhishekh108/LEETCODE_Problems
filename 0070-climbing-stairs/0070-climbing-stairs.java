5b3768aab2f528a299596a056f0420013cec06ca
Abhishekh Verma <kundanverma982155@gmail.com>
2026-09-11 23:02:58 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.29%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -5,7 +5,7 @@ class Solution {
         int dp[]= new int [n+1];
         Arrays.fill(dp,-1);
         dp[1]=1;
-        if(n>1) dp[2]=2;
+        if(n>1) {dp[2]=2;}
         for(int i=3;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
