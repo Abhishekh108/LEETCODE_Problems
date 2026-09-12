b99a12566582883a2bd6cc711da249dc4a7addf6
Abhishekh Verma <kundanverma982155@gmail.com>
2026-09-12 23:46:58 +0530

    Time: 0 ms (100%), Space: 42.3 MB (28.56%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,21 +1,33 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][]=new int [m][n];
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                dp[i][j]=-1;
+                if(i==0 || j==0){ // first row and first colum zero
+                    dp[i][j]=1;
+                }
+                else{
+                    dp[i][j]=dp[i][j-1]+dp[i-1][j];
+                }
             }
         }
-        return path(0,0,m,n,dp);
+        return dp[m-1][n-1];
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         dp[i][j]=-1;
+        //     }
+        // }
+        // return path(0,0,m,n,dp);
     }
-    private int path(int row, int col, int m , int n, int dp[][]){ //if we do here Integer then it will go pass by reference
-        if(row>=m || col>= n) return 0;
-        if(row== m-1 && col == n-1) return 1;
-        if(dp[row][col] !=-1) return dp[row][col];
-        int right=path(row,col+1,m,n,dp);
-        int down= path(row+1,col,m,n,dp);
-        return dp[row][col]=right+down;
-    }
+    // private int path(int row, int col, int m , int n, int dp[][]){ //if we do here Integer then it will go pass by reference
+    //     if(row>=m || col>= n) return 0;
+    //     if(row== m-1 && col == n-1) return 1;
+    //     if(dp[row][col] !=-1) return dp[row][col];
+    //     int right=path(row,col+1,m,n,dp);
+    //     int down= path(row+1,col,m,n,dp);
+    //     return dp[row][col]=right+down;
+    // }
 }
 
 // Synced seamlessly with LeetHub Pro
